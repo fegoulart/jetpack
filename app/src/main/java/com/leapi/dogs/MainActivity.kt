26d84bfef81d1b4c5d9c4dbf45c3f91a52d2df8d
Fernando Luiz Goulart <fernando.goulart@olxbr.com>
2020-04-10 10:09:48 -0300

    first commit

---
+++ app/src/main/java/com/leapi/dogs/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.leapi.dogs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+gti
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
